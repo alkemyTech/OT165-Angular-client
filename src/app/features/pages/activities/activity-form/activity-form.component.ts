bf1e47a3fd09a5632ee5cfbb9cec1f391b9874de
nacho <ignacio.bsso@hotmail.com>
2022-03-17 09:10:46 -0300

    'add-activity'

--- src/app/features/pages/activities/activity-form/activity-form.component.ts
+++ src/app/features/pages/activities/activity-form/activity-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-activity-form',
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-form.component.scss']
 })
 export class ActivityFormComponent implements OnInit {
-  title = 'base-ong-angular-client';
+
+  title = 'Apoyo Escolar Nivel Secundaria';
+  description = `Del mismo modo que en primaria, este taller es el corazón del área secundaria.
+  Serealizan talleres de lunes a viernes de 10 a 12 horas y de 16 a 18 horas en el
+  contraturno. Actualmente se encuentran inscriptos en el taller 50 adolescentes entre
+  13 y 20 años. Aquí los jóvenes se presentan con el material que traen del colegio y
+  una docente de la institución y un grupo de voluntarios los recibe para ayudarlos a
+  estudiar o hacer la tarea. Este espacio también es utilizado por los jóvenes como un
+  punto de encuentro y relación entre ellos y la institución.`
+
+  @Input() activity!: any
 
   constructor() { }
 
