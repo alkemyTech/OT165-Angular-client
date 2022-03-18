7b3f66e58e690c7f7417a1bad8c4b3541467246e
Agustina <agustina.perezserra@hotmail.com>
2022-03-18 10:49:25 -0300

    Back office module and routing added

--- src/app/backoffice/back-office-routing.module.ts
+++ src/app/backoffice/back-office-routing.module.ts
@@ -3,18 +3,9 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { OrganizationFormComponent } from "./pages/organization/organization-form/organization-form.component";
-import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
+
 
 const routes: Routes = [
-  {
-    path: "actividades",
-    component: ActivityFormComponent
-  },
-  {
-    path: "registro",
-    component: RegisterFormComponent,
-    pathMatch: "full",
-  },
   {
     path: "backoffice/organization/edit",
     component: OrganizationFormComponent
