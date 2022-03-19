8fb4e8a0f9431107f8243bf0adec2b788438b8b0
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-03-19 12:23:04 -0300

    Conflicts with main resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
   {
     path: 'backoffice',
+    pathMatch: 'full',
     loadChildren: () => import('./backoffice/backoffice.module').then(m => m.BackOfficeModule)
   },
   {
--- src/app/backoffice/pages/auth/login-form/login-form.component.ts
+++ src/app/backoffice/pages/auth/login-form/login-form.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { checkPattern } from '../custom.validators';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
+
+  formBuilder: FormBuilder = new FormBuilder();
+  form: FormGroup = this.formBuilder.group({
+    email: [null,[
+      Validators.required,
+      Validators.email
+    ]],
+    password: [null,[
+        Validators.required,
+        Validators.minLength(6),
+        checkPattern
+      ]]
+  });
 
   constructor() { }
 
-  ngOnInit(): void {
+  get email(): AbstractControl | null {
+    return this.form.get('email');
+  }
+  get password(): AbstractControl | null {
+    return this.form.get('password');
+  }
+
+  login(e:Event) {
+    e.preventDefault();
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    let object = {
+      email: this.email,
+      password: this.password
+    }
+    //here we should send the object to the http server
   }
 
 }
