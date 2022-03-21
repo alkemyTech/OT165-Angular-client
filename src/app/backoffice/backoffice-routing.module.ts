f03c5b72f0d115d8246f44c20f3ec47c36cbfde6
Lorenzo Spallione <82187204+lorenzo0895@users.noreply.github.com>
2022-03-21 09:33:46 -0300

    Feat/ot165 26 (#12)
    * Conflicts with main resolved
    
    * Fix for PR

--- src/app/backoffice/backoffice-routing.module.ts
+++ src/app/backoffice/backoffice-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { ActivityFormComponent } from "./pages/activities/activity-form/activity-form.component";
+import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
 import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
 import { OrganizationFormComponent } from "./pages/organization/organization-form/organization-form.component";
 
@@ -10,7 +11,8 @@ const routes: Routes = [
     path: '',
     children: [
       { path: 'actividades', component: ActivityFormComponent },
-      { path: 'registro', component: RegisterFormComponent },
+      { path: 'registro', component: RegisterFormComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginFormComponent, pathMatch: 'full' },
       { path: 'organization/edit', component: OrganizationFormComponent },
       { path: '**', redirectTo: 'actividades'}
     ]
