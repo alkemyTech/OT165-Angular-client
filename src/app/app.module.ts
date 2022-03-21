27beb69ede3ead62283cbd9e49f5a79b9ea6c417
Ignacio Alvarez <36280731+nacho992@users.noreply.github.com>
2022-03-21 16:07:16 -0300

    animationBroserModule in app module (#23)
    * animationBroserModule in app module
    
    * logo quitado
    
    * logo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 
@@ -10,7 +11,7 @@ import { BackOfficeModule } from './backoffice/backoffice.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BackOfficeModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BackOfficeModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
