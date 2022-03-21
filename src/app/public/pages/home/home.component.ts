375d7815ed08ce1b26e3fb4a111c36e373ed593f
Esteban Fernandez <efernandezdev@gmail.com>
2022-03-20 21:18:59 -0300

    add placeholders home and text

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  public welcome: string = "Texto de Bienvenida";
+  
+  //Get data from public endpoint
+  
+  public welcome: string =
+    "En Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio generando procesos de crecimiento y inserción social.";
 
   constructor() {}
 
