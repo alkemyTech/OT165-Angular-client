236368b5bdf7901d7297702037aaaa934b5d9784
DanieLazo17 <daniel.lazo92@gmail.com>
2022-03-22 11:19:49 -0300

    fix-duplicated-module

--- src/app/backoffice/backoffice.module.ts
+++ src/app/backoffice/backoffice.module.ts
@@ -19,7 +19,6 @@ import { OrganizationComponent } from "./pages/organization/organization-view/or
 
 import { InputTextModule } from "primeng/inputtext";
 import { ButtonModule } from "primeng/button";
-import { FileUploadModule } from "primeng/fileupload";
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "../app-routing.module";
