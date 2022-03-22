1b5afaf05f7c1303f85235108b7635f82268af02
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-03-22 15:51:43 -0300

    Name interface changed for Activity

--- src/app/public/pages/activities/details/detail.component.ts
+++ src/app/public/pages/activities/details/detail.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
-
-
-
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
+import { ActivityService } from "src/app/services/activity/activity.service";
+import { Activity } from "src/app/shared/models/activity";
 
 @Component({
-  selector: 'app-detail',
-  templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  selector: "app-detail",
+  templateUrl: "./detail.component.html",
+  styleUrls: ["./detail.component.scss"],
 })
 export class DetailComponent implements OnInit, OnDestroy {
-
   subscription: Subscription = new Subscription();
   activity: Activity = {
     id: 0,
-    name: 'Actividad no encontrada'
-  }
+    name: "Actividad no encontrada",
+  };
 
-  constructor(private service: ActivityService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private service: ActivityService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.subscription = this.activatedRoute.params.subscribe(params => {
-      this.service.getActivity(params.id).subscribe(activity => {
+    this.subscription = this.activatedRoute.params.subscribe((params) => {
+      this.service.getActivity(params.id).subscribe((activity) => {
         this.activity = activity;
-      })
-    })
+      });
+    });
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
-
 }
