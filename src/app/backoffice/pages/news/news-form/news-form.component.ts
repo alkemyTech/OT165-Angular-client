66a7c92481166e767f5dbc11e7a6117bfae22120
DanieLazo17 <daniel.lazo92@gmail.com>
2022-03-23 19:47:22 -0300

    news form added

--- src/app/backoffice/pages/news/news-form/news-form.component.ts
+++ src/app/backoffice/pages/news/news-form/news-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { News } from 'src/app/backoffice/models/news';
+import { NewsService } from 'src/app/backoffice/services/news.service';
 
 @Component({
   selector: 'app-news-form',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() news!: News;
+  actionType: string = 'Crear';
+  buttonAction: string = 'Crear';
+  paramID!:number;
 
-  ngOnInit(): void {
+  constructor(private serviceNews: NewsService, private route: ActivatedRoute) {
+    this.paramID = this.route.snapshot.params['id'] != undefined ? this.route.snapshot.params['id'] : 0;    
   }
 
+  newsForm = new FormGroup({
+    name: new FormControl('', [Validators.minLength(4), Validators.required]),
+    content: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+  });
+
+  get name(){ return this.newsForm.get('name')!; }
+  get content(){ return this.newsForm.get('content')!; }
+  get image(){ return this.newsForm.get('image')!; }
+    
+  maxFileSize: number = 2000000;
+  uploadedFile: any = null;  
+
+  ngOnInit(): void {}
 }
