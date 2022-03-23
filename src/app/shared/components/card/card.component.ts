a397e9e53bc94bdc10d0a6b0d7b4281cdb630261
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-03-23 20:16:29 -0300

    card made an <a> tag with new attribute href

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -10,11 +11,17 @@ export class CardComponent {
   @Input() imageSrc: string = '../../../../assets/images/image-not-found.png';
   @Input() title: string = '';
   @Input() description: string = '';
+  @Input() href: string = '/';
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   errorImageSrc () {
     this.imageSrc = '../../../../assets/images/image-not-found2.png';
+  }
+
+  onClick(e: Event) {
+    e.preventDefault();
+    this.route.navigate([this.href]);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 //Own Components
-import { LogoComponent } from "./logo/logo.component";
+import { LogoComponent } from "./components/logo/logo.component";
 import { TitleComponent } from "./components/title/title.component";
 import { CardComponent } from './components/card/card.component';
 
