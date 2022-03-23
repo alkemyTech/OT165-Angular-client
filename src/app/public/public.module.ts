22e5965364e13af12c7a46faf3861bdfe5490de3
Lorenzo Spallione <82187204+lorenzo0895@users.noreply.github.com>
2022-03-23 11:33:08 -0300

    Fix/main (#31)
    * Bug fixed
    
    * Change path listNews on imports of Module

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -7,7 +7,7 @@ import { PublicRoutingModule } from "./public-routing.module";
 import { HomeComponent } from "./pages/home/home.component";
 import { PublicComponent } from "./public.component";
 import { NavComponent } from "./shared/nav/nav.component";
-import { ListnewsComponent } from "./components/listnews/listnews.component";
+import { ListnewsComponent } from "./components/listNews/listnews.component";
 import { SliderComponent } from "./components/slider/slider.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { AboutComponent } from "./pages/aboutUs/about.component";
@@ -37,7 +37,8 @@ import { CarouselModule } from "./components/carousel/carousel.module";
     ThanksComponent,
     DonationAmountComponent,
     ImageCurvedComponent,
-    DetailComponent
+    DetailComponent,
+    AboutComponent,
   ],
   exports: [
     HomeComponent,
