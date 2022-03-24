734bc0b4cded969183bcc16e1126d0819d74cf90
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-03-24 20:13:23 -0300

    quotation marks changed

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,20 +1,20 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "app-card",
-  templateUrl: "./card.component.html",
-  styleUrls: ["./card.component.scss"],
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  @Input() imageSrc: string = "../../../../assets/images/image-not-found.png";
-  @Input() title: string = "";
-  @Input() description: string = "";
+  @Input() imageSrc: string = '../../../../assets/images/image-not-found.png';
+  @Input() title: string = '';
+  @Input() description: string = '';
   @Input() navigateTo?: string;
 
   constructor() {
   }
 
   errorImageSrc() {
-    this.imageSrc = "../../../../assets/images/image-not-found2.png";
+    this.imageSrc = '../../../../assets/images/image-not-found2.png';
   }
 }
