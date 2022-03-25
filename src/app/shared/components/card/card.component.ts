ce1a33b957d057708f108e0a5bb125842a2ee660
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-03-25 12:02:07 -0300

    description rendered instead of showing plain text

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CardComponent {
   @Input() imageSrc: string = '../../../../assets/images/image-not-found.png';
   @Input() title: string = '';
   @Input() description: string = '';
-  @Input() navigateTo?: string;
+  @Input() detailsLink?: string;
 
   constructor() { }
 
