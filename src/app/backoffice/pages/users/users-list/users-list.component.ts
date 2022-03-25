0a4ac6f1af5c475d13ffa6185e38a6216775229d
DanieLazo17 <daniel.lazo92@gmail.com>
2022-03-25 19:30:25 -0300

    user service added

--- src/app/backoffice/pages/users/users-list/users-list.component.ts
+++ src/app/backoffice/pages/users/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/backoffice/models/user';
+import { UserService } from 'src/app/backoffice/services/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
+  users!:Array<User>;
 
-  constructor() { }
+  constructor(private servicioUser: UserService) { 
+    this.servicioUser.getUsers().subscribe(
+      response => { this.showUsers(response)}
+    )
+  }
 
   ngOnInit(): void {
   }
 
+  showUsers(response: any){
+    this.users = <Array<User>>response.data;
+    console.log(this.users[1]);
+  }
 }
