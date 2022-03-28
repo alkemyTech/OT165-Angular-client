704356ee8b637b6bb2856dab618e79b142e1f577
Lorenzo Spallione <82187204+lorenzo0895@users.noreply.github.com>
2022-03-28 16:02:48 -0300

    Interceptor added (#43)

---
+++ src/app/backoffice/services/token-interceptor.service.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('token');
+    const tokenHeader = req.clone({
+      setHeaders: {
+        Authorization : 'Bearer ' + token
+      }
+    });
+    return next.handle(tokenHeader);
+  }
+
+}
