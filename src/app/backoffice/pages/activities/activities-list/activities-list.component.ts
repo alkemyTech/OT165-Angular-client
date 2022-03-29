731130060d3bf3dd7209e72669c5b2363ecfa5dd
Esteban Fernandez <efernandezdev@gmail.com>
2022-03-29 00:59:26 -0300

    Responsive Design

--- src/app/backoffice/pages/activities/activities-list/activities-list.component.ts
+++ src/app/backoffice/pages/activities/activities-list/activities-list.component.ts
@@ -10,7 +10,9 @@ import { ConfirmationService, MessageService } from "primeng/api";
 export class ActivitiesListComponent implements OnInit {
   public items!: any;
 
-  titlesCol: string[] = ["Nombre", "Imagen", "Creada"];
+  newData: any[] = [];
+
+  titlesCol: string[] = ["Nombre", "Imagen", "Creado"];
 
   activities!: Array<{
     id: number;
@@ -23,7 +25,7 @@ export class ActivitiesListComponent implements OnInit {
     created_at: string;
     updated_at: string;
     deleted_at: string | null;
-    group_id: string | null;
+    group_id: string | number | null;
   }>;
 
   constructor(private messageService: MessageService) {}
@@ -31,86 +33,111 @@ export class ActivitiesListComponent implements OnInit {
   ngOnInit(): void {
     this.activities = [
       {
-        id: 1599,
-        name: "homer",
+        id: 1626,
+        name: "Apoyo Escolar nivel Secundario",
         slug: null,
-        description: "<p>dsadsada</p>",
-        image: "http://ongapi.alkemy.org/storage/kJIcWM5qZs.jpeg",
+        description:
+          "<p>Del mismo modo que en la primaria, este taller es el coraz\u00f3n del \u00e1rea secundaria. Este espacio tambi\u00e9n es utilizado por los j\u00f3venes como un punto de encuentro.</p>",
+        image: "http://ongapi.alkemy.org/storage/AYZ08pid6V.jpeg",
         user_id: null,
         category_id: null,
-        created_at: "2022-03-25T23:57:59.000000Z",
-        updated_at: "2022-03-27T21:42:08.000000Z",
+        created_at: "2022-03-28T13:48:40.000000Z",
+        updated_at: "2022-03-28T13:48:40.000000Z",
         deleted_at: null,
-        group_id: null,
+        group_id: 42,
       },
       {
-        id: 1600,
-        name: "asdda",
+        id: 1627,
+        name: "Tutor\u00edas",
         slug: null,
-        description: "<p>asdada</p>",
-        image: "http://ongapi.alkemy.org/storage/ISIKI0EIUi.png",
+        description:
+          "<p>Es un programa destinado a j\u00f3venes a partir del tercer a\u00f1o de secundaria, cuyo objetivo es garantizar su permanencia en el colegio y servir de gu\u00eda y soporte.&nbsp;</p>",
+        image: "http://ongapi.alkemy.org/storage/U2K44C9Z0D.jpeg",
         user_id: null,
         category_id: null,
-        created_at: "2022-03-26T00:47:00.000000Z",
-        updated_at: "2022-03-26T00:47:00.000000Z",
+        created_at: "2022-03-28T13:52:57.000000Z",
+        updated_at: "2022-03-28T13:52:57.000000Z",
         deleted_at: null,
-        group_id: null,
+        group_id: 42,
       },
-      // {
-      //   id: 1602,
-      //   name: "Día de camping",
-      //   slug: null,
-      //   description: "<p>Día de camping familiar</p>",
-      //   image: "http://ongapi.alkemy.org/storage/HG14tKkdzI.jpeg",
-      //   user_id: null,
-      //   category_id: null,
-      //   created_at: "2022-03-26T15:22:25.000000Z",
-      //   updated_at: "2022-03-26T15:22:25.000000Z",
-      //   deleted_at: null,
-      //   group_id: null,
-      // },
-      // {
-      //   id: 1603,
-      //   name: "Clases de fotografía",
-      //   slug: null,
-      //   description: "<p>Clases de fotografía para niños.</p>",
-      //   image: "http://ongapi.alkemy.org/storage/3n4qXVlsJS.jpeg",
-      //   user_id: null,
-      //   category_id: null,
-      //   created_at: "2022-03-26T15:22:55.000000Z",
-      //   updated_at: "2022-03-26T15:22:55.000000Z",
-      //   deleted_at: null,
-      //   group_id: null,
-      // },
-      // {
-      //   id: 1604,
-      //   name: "Día de campo",
-      //   slug: null,
-      //   description: "<p>Día de campamento con los carajitos</p>",
-      //   image: "http://ongapi.alkemy.org/storage/vxndro9ccJ.png",
-      //   user_id: null,
-      //   category_id: null,
-      //   created_at: "2022-03-26T15:23:17.000000Z",
-      //   updated_at: "2022-03-27T03:46:57.000000Z",
-      //   deleted_at: null,
-      //   group_id: null,
-      // },
-      // {
-      //   id: 1615,
-      //   name: "Día de campo",
-      //   slug: null,
-      //   description: "<p>jesus es el mejor</p>",
-      //   image: "http://ongapi.alkemy.org/storage/TkIslC93B7.jpeg",
-      //   user_id: null,
-      //   category_id: null,
-      //   created_at: "2022-03-27T04:13:06.000000Z",
-      //   updated_at: "2022-03-27T04:13:06.000000Z",
-      //   deleted_at: null,
-      //   group_id: null,
-      // },
+      {
+        id: 1628,
+        name: "Taller Arte y Cuentos",
+        slug: null,
+        description:
+          "<p>Taller literario y de manualidades que se realiza permanentemente.</p>",
+        image: "http://ongapi.alkemy.org/storage/77cynab6SQ.jpeg",
+        user_id: null,
+        category_id: null,
+        created_at: "2022-03-28T13:55:23.000000Z",
+        updated_at: "2022-03-28T13:55:23.000000Z",
+        deleted_at: null,
+        group_id: 42,
+      },
+      {
+        id: 1629,
+        name: "Paseos Recreativos y Educativos",
+        slug: null,
+        description:
+          "<p>Estos paseos est\u00e1n pensados para promover la participaci\u00f3n y sentido de permanencia de los ni\u00f1os, ni\u00f1as y adolescentes del \u00e1rea educativa.</p>",
+        image: "http://ongapi.alkemy.org/storage/XNFwl4vYjC.jpeg",
+        user_id: null,
+        category_id: null,
+        created_at: "2022-03-28T13:58:31.000000Z",
+        updated_at: "2022-03-28T13:58:31.000000Z",
+        deleted_at: null,
+        group_id: 42,
+      },
+      {
+        id: 1630,
+        name: "Jornada de vacunaci\u00f3n",
+        slug: null,
+        description:
+          "<p>Protecci\u00f3n viral para los m\u00e1s peque\u00f1os de la casa, Somos M\u00e1s realiza semestralmente un gran plan de vacunaci\u00f3n</p>",
+        image: "http://ongapi.alkemy.org/storage/5pDwVCc7Gu.jpeg",
+        user_id: null,
+        category_id: null,
+        created_at: "2022-03-28T14:45:00.000000Z",
+        updated_at: "2022-03-28T14:45:00.000000Z",
+        deleted_at: null,
+        group_id: 42,
+      },
+      {
+        id: 1631,
+        name: "Juegos din\u00e1micos",
+        slug: null,
+        description:
+          "<p>Diversi\u00f3n para los ni\u00f1os a trav\u00e9s de juegos din\u00e1micos, promoviendo as\u00ed los valores de la uni\u00f3n, jugando en equipos</p>",
+        image: "http://ongapi.alkemy.org/storage/tF0hZSHbJC.jpeg",
+        user_id: null,
+        category_id: null,
+        created_at: "2022-03-28T14:49:57.000000Z",
+        updated_at: "2022-03-28T14:49:57.000000Z",
+        deleted_at: null,
+        group_id: 42,
+      },
+      {
+        id: 1632,
+        name: "TITULO",
+        slug: null,
+        description: "<p>ACTIVIDAD</p>",
+        image: "http://ongapi.alkemy.org/storage/YZrJygTQKt.png",
+        user_id: null,
+        category_id: null,
+        created_at: "2022-03-28T20:30:17.000000Z",
+        updated_at: "2022-03-28T20:30:17.000000Z",
+        deleted_at: null,
+        group_id: 42,
+      },
     ];
-    console.log(this.activities);
-    this.showActivities(this.activities);
+
+    for (let index = 0; index < this.activities.length; index++) {
+      let { id, image, name, created_at } = this.activities[index];
+      let cleanData = { id, image, name, createdAt: created_at };
+      this.newData.push(cleanData);
+    }
+
+    this.showActivities(this.newData);
   }
 
   showActivities(data: any) {
