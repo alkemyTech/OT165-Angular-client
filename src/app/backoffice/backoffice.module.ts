2501221ce1419420fabbc437fd9171227b29ed1b
Esteban Fernandez <86627778+efernandezdev@users.noreply.github.com>
2022-03-30 15:44:19 -0300

    Feat/ot165 62 (#47)
    * Create a component activities list
    
    * Responsive Design
    
    * Clean code
    
    * Add path to routing module
    
    * Add new paths in routing
    
    * Fix to PR

--- src/app/backoffice/backoffice.module.ts
+++ src/app/backoffice/backoffice.module.ts
@@ -1,4 +1,3 @@
-import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
@@ -13,15 +12,21 @@ import { BackofficeComponent } from "./pages/backoffice/backoffice.component";
 //Others Modules
 import { ButtonModule } from "primeng/button";
 import { CKEditorModule } from "ckeditor4-angular";
+import { ConfirmDialogModule } from "primeng/confirmdialog";
 import { DialogModule } from "primeng/dialog";
 import { DropdownModule } from "primeng/dropdown";
 import { FileUploadModule } from "primeng/fileupload";
 import { InputTextModule } from "primeng/inputtext";
 import { InputTextareaModule } from "primeng/inputtextarea";
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { ToolbarModule } from "primeng/toolbar";
 
 //Own Components
+import { ActivitiesListComponent } from "./pages/activities/activities-list/activities-list.component";
 import { ActivityFormComponent } from "./pages/activities/activity-form/activity-form.component";
 import { CategoriesFormComponent } from "./pages/categories/categories-form/categories-form.component";
+import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
 import { ControlComponent } from "./pages/backoffice/control/control.component";
 import { HomeFormComponent } from "./pages/home/home-form/home-form.component";
 import { MembersComponent } from "./pages/members/members.component";
@@ -29,59 +34,55 @@ import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 import { OrganizationComponent } from "./pages/organization/organization-view/organization.component";
 import { OrganizationFormComponent } from "./pages/organization/organization-form/organization-form.component";
 import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.component";
-import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
-import { UserFormComponent } from "./pages/users/user-form/user-form.component";
 import { SlidesListComponent } from "./pages/slides/slides-list/slides-list.component";
 import { TableComponent } from "./shared/components/table/table.component";
+import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
+import { UserFormComponent } from "./pages/users/user-form/user-form.component";
 import { UsersListComponent } from "./pages/users/users-list/users-list.component";
 
-import { ToolbarModule } from "primeng/toolbar";
-import { ConfirmDialogModule } from "primeng/confirmdialog";
-import { ToastModule } from "primeng/toast";
-import { TableModule } from "primeng/table";
 import { SharedModule } from "../shared/shared.module";
 import { TokenInterceptorService } from "./services/token-interceptor.service";
 
 @NgModule({
   declarations: [
+    ActivitiesListComponent,
     ActivityFormComponent,
-    HomeFormComponent,
-    CategoriesFormComponent,
-    NewsFormComponent,
-    SlidesFormComponent,
-    MembersComponent,
-    TestimonialFormComponent,
-    UserFormComponent,
-    OrganizationFormComponent,
-    OrganizationComponent,
     BackofficeComponent,
+    CategoriesFormComponent,
+    CategoryListComponent,
     ControlComponent,
-    UsersListComponent,
+    HomeFormComponent,
+    MembersComponent,
+    NewsFormComponent,
+    OrganizationComponent,
+    OrganizationFormComponent,
+    SlidesFormComponent,
     SlidesListComponent,
     TableComponent,
-    CategoryListComponent
+    TestimonialFormComponent,
+    UserFormComponent,
+    UsersListComponent,
   ],
   imports: [
+    BackOfficeRoutingModule,
+    ButtonModule,
+    CKEditorModule,
+    CommonModule,
     ConfirmDialogModule,
+    DialogModule,
+    DropdownModule,
+    FileUploadModule,
+    FormsModule,
+    HttpClientModule,
+    InputTextModule,
+    InputTextareaModule,
+    ReactiveFormsModule,
+    RouterModule,
+    RxReactiveFormsModule,
+    SharedModule,
     TableModule,
     ToastModule,
     ToolbarModule,
-    CommonModule,
-    BackOfficeRoutingModule,
-    RouterModule,
-    ReactiveFormsModule,
-    FormsModule,
-    DropdownModule,
-    InputTextModule,
-    ButtonModule,
-    RxReactiveFormsModule,
-    CKEditorModule,
-    FileUploadModule,
-    DialogModule,
-    HttpClientModule,
-    SharedModule,
-    DropdownModule,
-    InputTextareaModule,
   ],
   providers: [
     {
