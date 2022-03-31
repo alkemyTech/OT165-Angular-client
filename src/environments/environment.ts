95719f50e64afa545876bae871e6678e9673e9e7
Agustina <agustina.perezserra@hotmail.com>
2022-03-31 12:09:39 -0300

    Members enviroment variable created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   BASE_URL_API: 'https://ongapi.alkemy.org/api/',
   API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
+  API_URL_MEMBERS: 'https://ongapi.alkemy.org/api/members',
 };
 
 /*
