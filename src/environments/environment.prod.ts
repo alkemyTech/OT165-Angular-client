5dfef53126324d75db61438ab3c3aa08a3405cd7
Esteban Fernandez <efernandezdev@gmail.com>
2022-04-01 12:21:15 -0300

    Enviroment activities

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
 export const environment = {
   production: true,
-  BASE_URL_API: 'https://ongapi.alkemy.org/api/',
-  API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
-  API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,6 @@
 
 export const environment = {
   production: false,
-  BASE_URL_API: 'https://ongapi.alkemy.org/api/',
-  API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
-  API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
 };
 
 /*
