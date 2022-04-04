3f57697adfde1288674a187a81bff7bffb343b6f
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-04-03 21:00:54 -0300

    API_URL added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   BASE_URL_API: 'https://ongapi.alkemy.org/api/',
   API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
   API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
+  API_URL_ORGANIZATION: 'https://ongapi.alkemy.org/api/organization',
 };
