6a6f8f83148d8e3d46716019bf45712de1b2a9d7
Esteban Fernandez <86627778+efernandezdev@users.noreply.github.com>
2022-04-05 12:56:02 -0300

    Feat/ot165 84 (#67)
    * fix module backoffice
    
    * Enviroment activities
    
    * Config gitignore
    
    * Extend service
    
    * Solved requests changes
    
    * Add Base Url
    
    * Solved changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  BASE_URL_API: 'https://ongapi.alkemy.org/api/',
-  API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
-  API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
+  BASE_URL_API: "https://ongapi.alkemy.org/api/",
+  API_URL_SLIDES: "https://ongapi.alkemy.org/api/slides",
+  API_URL_USERS: "https://ongapi.alkemy.org/api/users",
+  API_URL_ACTIVITIES: "https://ongapi.alkemy.org/api/users",
 };
