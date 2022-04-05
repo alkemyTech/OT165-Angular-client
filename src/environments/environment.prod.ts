2b839ad806ed6c97963787e1641d4e856c6b290c
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-04-05 14:43:26 -0300

    PR fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,8 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  BASE_URL_API: 'https://ongapi.alkemy.org/api/',
-  API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
-  API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
-  API_URL_ORGANIZATION: 'https://ongapi.alkemy.org/api/organization',
-=======
   BASE_URL_API: "https://ongapi.alkemy.org/api/",
   API_URL_SLIDES: "https://ongapi.alkemy.org/api/slides",
   API_URL_USERS: "https://ongapi.alkemy.org/api/users",
   API_URL_ACTIVITIES: "https://ongapi.alkemy.org/api/users",
->>>>>>> main
-};
+  API_URL_ORGANIZATION: 'https://ongapi.alkemy.org/api/organization',
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,11 @@
 
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  BASE_URL_API: 'https://ongapi.alkemy.org/api/',
-  API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
-  API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
-  API_URL_ORGANIZATION: 'https://ongapi.alkemy.org/api/organization',
-=======
   BASE_URL_API: "https://ongapi.alkemy.org/api/",
   API_URL_SLIDES: "https://ongapi.alkemy.org/api/slides",
   API_URL_USERS: "https://ongapi.alkemy.org/api/users",
   API_URL_ACTIVITIES: "https://ongapi.alkemy.org/api/users",
->>>>>>> main
+  API_URL_ORGANIZATION: 'https://ongapi.alkemy.org/api/organization',
 };
 
 /*
