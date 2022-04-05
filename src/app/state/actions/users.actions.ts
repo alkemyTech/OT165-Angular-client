f60b2afcbf7e877fedafb08fb4bfd501f80c86a4
DanieLazo17 <daniel.lazo92@gmail.com>
2022-04-05 16:13:09 -0300

    delete skeleton and format document

--- src/app/state/actions/users.actions.ts
+++ src/app/state/actions/users.actions.ts
@@ -1,30 +1,29 @@
-import { createAction, props } from '@ngrx/store';
-import { User } from 'src/app/backoffice/models/user';
+import { createAction, props } from "@ngrx/store";
+import { User } from "src/app/backoffice/models/user";
 
-export const getUsers = createAction(
-    '[Users List] Get users'
-);
+export const getUsers = createAction("[Users List] Get users");
 
 export const getUsersSuccess = createAction(
-    '[Users List] Get users success',
-    props<{ users: Array<User> }>()
+  "[Users List] Get users success",
+  props<{ users: Array<User> }>()
 );
 
 export const deleteUser = createAction(
-    '[Users List] Delete user',
-    props<{ id: number }>()
+  "[Users List] Delete user",
+  props<{ id: number }>()
 );
 
 export const deleteUserSuccess = createAction(
-    '[Users List] Delete user success',
-    props<{ id: number }>()    
+  "[Users List] Delete user success",
+  props<{ id: number }>()
 );
 
 export const addUser = createAction(
-    '[Users List] Add user',    
-    props<{ user: User }>()
-  );
-  export const addUserSuccess = createAction(
-    '[Users List] Add user success',
-    props<{ user: User }>()    
-  );
+  "[Users List] Add user",
+  props<{ user: User }>()
+);
+
+export const addUserSuccess = createAction(
+  "[Users List] Add user success",
+  props<{ user: User }>()
+);
