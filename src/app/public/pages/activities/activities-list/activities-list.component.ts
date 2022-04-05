2917405f11dd83012ddd9b03830fd0d7f1d79276
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-04-05 10:02:41 -0300

    Dialog and ProgressBar added to Activity and DetailsActivity

--- src/app/public/pages/activities/activities-list/activities-list.component.ts
+++ src/app/public/pages/activities/activities-list/activities-list.component.ts
@@ -1,37 +1,49 @@
-import { ActivityService } from 'src/app/services/activity/activity.service';
-import { Component, OnInit } from '@angular/core';
-import { Activity } from 'src/app/shared/models/Activity';
+import { ActivityService } from "src/app/services/activity/activity.service";
+import { Component, OnInit } from "@angular/core";
+import { Activity } from "src/app/shared/models/Activity";
+import { DialogService } from "src/app/shared/components/dialog/dialog.service";
 
 @Component({
-  selector: 'app-activities-list',
-  templateUrl: './activities-list.component.html',
-  styleUrls: ['./activities-list.component.scss']
+  selector: "app-activities-list",
+  templateUrl: "./activities-list.component.html",
+  styleUrls: ["./activities-list.component.scss"],
 })
 export class ActivitiesListComponent implements OnInit {
-
+  isLoading!: boolean;
   activities: Activity[] = [];
   filteredActivities: Activity[] = [];
   itemCount: number = 0;
-  
 
-  constructor(private activityService: ActivityService) { }
+  constructor(
+    private activityService: ActivityService,
+    private dialogService: DialogService,
+  ) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.getActivities();
   }
 
   getActivities() {
     this.activityService.getActivities().subscribe({
-      next: async (activities) => {        
-        this.activities = await activities;               
-        this.fillArray();        
-      }
-    })
-  } 
-
-  fillArray() {   
-    if(this.itemCount != this.activities.length) this.itemCount += 6;    
-    this.filteredActivities = this.activities.slice(0,this.itemCount);        
+      next: (activities) => {
+        this.activities = activities;
+        this.fillArray();
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.dialogService.add({
+          type: "error",
+          title: "Error en el servidor",
+          detail: "No pudo conectarse con el servidor",
+        });
+        this.isLoading = false;
+      },
+    });
   }
 
+  fillArray() {
+    if (this.itemCount != this.activities.length) this.itemCount += 6;
+    this.filteredActivities = this.activities.slice(0, this.itemCount);
+  }
 }
