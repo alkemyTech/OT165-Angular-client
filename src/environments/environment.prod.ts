4dda2bc575f1969d01ed94edef9f984033baaff5
Esteban Fernandez <efernandezdev@gmail.com>
2022-04-09 18:17:57 -0300

    Add progress and dialog error server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
   BASE_URL_API: 'https://ongapi.alkemy.org/api/',
   API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
   API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
-  API_URL_ACTIVITIES: 'https://ongapi.alkemy.org/api/users',
+  API_URL_ACTIVITIES: 'https://ongapi.alkemy.org/api/activities',
   API_URL_ORGANIZATION: 'https://ongapi.alkemy.org/api/organization',
   API_URL_MEMBERS: 'https://ongapi.alkemy.org/api/members',
   API_URL_NEWS: 'https://ongapi.alkemy.org/api/news',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
   API_URL_SLIDES: 'https://ongapi.alkemy.org/api/slides',
   API_URL_USERS: 'https://ongapi.alkemy.org/api/users',
   API_URL_CATEGORIES: 'https://ongapi.alkemy.org/api/categories',
-  API_URL_ACTIVITIES: 'https://ongapi.alkemy.org/api/users',
+  API_URL_ACTIVITIES: 'https://ongapi.alkemy.org/api/activities',
   API_URL_ORGANIZATION: 'https://ongapi.alkemy.org/api/organization',
   API_URL_MEMBERS: 'https://ongapi.alkemy.org/api/members',
   API_URL_NEWS: 'https://ongapi.alkemy.org/api/news',
