2156cdb673515252731f57193cdc0c74df9c82c4
DanieLazo17 <daniel.lazo92@gmail.com>
2022-04-08 21:53:03 -0300

    tileLayer and marker added

--- src/app/public/pages/contact/contact.component.ts
+++ src/app/public/pages/contact/contact.component.ts
@@ -1,68 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Contact } from 'src/app/shared/models/contact';
-import { ContactService } from 'src/app/public/services/contact/contact.service';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Contact } from "src/app/shared/models/contact";
+import { ContactService } from "src/app/public/services/contact/contact.service";
 
-import { DialogService } from 'src/app/shared/components/dialog/dialog.service';
-import { latLng, Map, marker, tileLayer } from 'leaflet';
+import { DialogService } from "src/app/shared/components/dialog/dialog.service";
+import { divIcon, Icon, Map, marker, tileLayer } from "leaflet";
+import { OrganizationService } from "src/app/services/organization/organization.service";
+import { Organization } from "src/app/backoffice/models/organization";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss'],
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
-export class ContactComponent implements OnInit{
+export class ContactComponent{
+  organization!: Organization;
+  latOrganization:number = -34.59755;
+  lonOrganization:number = -58.37742;
+
   constructor(
     private serviceContact: ContactService,
+    private serviceOrganization: OrganizationService,
     private dialogService: DialogService
-  ) {}
+  ) {
+    this.organization = new Organization();
+    this.serviceOrganization.getAll().subscribe((response) => {
+      this.loadOrganization(response);
+    });
+  }
 
   contactForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [
+    name: new FormControl("", [Validators.required]),
+    email: new FormControl("", [
       Validators.pattern(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/),
       Validators.required,
     ]),
-    phone: new FormControl('', [
+    phone: new FormControl("", [
       Validators.pattern(/^([0-9]{8,})*$/),
       Validators.required,
     ]),
-    message: new FormControl('', [Validators.required]),
-  });
+    message: new FormControl("", [Validators.required]),
+  });  
 
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-      })
-    ],
-    zoom: 7,
-    center: latLng([ 46.879966, -121.726909 ])
-  };
+  loadMap() {
+    const map = new Map("map").setView([this.latOrganization, this.lonOrganization], 15);
 
-  ngOnInit(): void {
-    var map = new Map('map').setView([51.505, -0.09], 13);
-
-    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    const myIcon = new Icon({
+      iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+      shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]      
+    });
+
+    marker([this.latOrganization, this.lonOrganization], { icon: myIcon })
+      .addTo(map)
+      .bindPopup(this.organization.name + "<br>" + this.organization.address)
       .openPopup();
   }
 
+  loadOrganization(response: any) {
+    this.organization = response;
+    this.loadMap();
+  }
+
   get name() {
-    return this.contactForm.get('name')!;
+    return this.contactForm.get("name")!;
   }
   get email() {
-    return this.contactForm.get('email')!;
+    return this.contactForm.get("email")!;
   }
   get phone() {
-    return this.contactForm.get('phone')!;
+    return this.contactForm.get("phone")!;
   }
   get message() {
-    return this.contactForm.get('message')!;
+    return this.contactForm.get("message")!;
   }
 
   sendMessage() {
@@ -74,10 +91,10 @@ export class ContactComponent implements OnInit{
     };
 
     this.serviceContact.createContact(contact).subscribe(
-      (response) => {        
+      (response) => {
         this.messageSuccess();
       },
-      (error) => {        
+      (error) => {
         this.messageError();
       }
     );
@@ -89,18 +106,18 @@ export class ContactComponent implements OnInit{
 
   messageSuccess() {
     this.dialogService.add({
-      type: 'success',
-      title: 'Respuesta exitosa',
-      detail: 'Su mensaje ha sido enviado con éxito!',
+      type: "success",
+      title: "Respuesta exitosa",
+      detail: "Su mensaje ha sido enviado con éxito!",
       life: 3000,
     });
   }
 
   messageError() {
     this.dialogService.add({
-      type: 'error',
-      title: 'Ha ocurrido un error',
-      detail: 'El mensaje no pudo ser enviado, por favor intente nuevamente.',
+      type: "error",
+      title: "Ha ocurrido un error",
+      detail: "El mensaje no pudo ser enviado, por favor intente nuevamente.",
       life: 3000,
     });
   }
