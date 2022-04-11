ad23fc822ba620627a92b6085227223d9bc2e07f
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-04-11 16:37:32 -0300

    Header Component for backoffice

--- src/app/backoffice/backoffice.module.ts
+++ src/app/backoffice/backoffice.module.ts
@@ -46,6 +46,7 @@ import { TokenInterceptorService } from "./services/token-interceptor.service";
 import { SpinnerComponent } from "./components/spinner/spinner.component";
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { BackofficeListComponent } from "./pages/backoffice-list/backoffice.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -70,6 +71,7 @@ import { BackofficeListComponent } from "./pages/backoffice-list/backoffice.comp
     SpinnerComponent,
     SkeletonComponent,
     SideBarComponent,
+    HeaderComponent,
   ],
   imports: [
     ProgressSpinnerModule,
--- src/app/backoffice/components/side-bar/side-bar.component.ts
+++ src/app/backoffice/components/side-bar/side-bar.component.ts
@@ -5,36 +5,19 @@ import { Component, Input, OnInit } from "@angular/core";
   templateUrl: "./side-bar.component.html",
   styleUrls: ["./side-bar.component.scss"],
 })
-export class SideBarComponent implements OnInit {
+export class SideBarComponent {
 
   @Input() itemsList!: any[]
 
   constructor() {}
 
-  ngOnInit(): void {
-    
+  menuBtnChange() {
     let sidebar = document.querySelector(".sidebar");
-    let closeBtn = document.querySelector("#btn");
-    let searchBtn = document.querySelector(".bx-search");
 
-    closeBtn?.addEventListener("click", () => {
+    if (sidebar?.classList.contains("open")) {
+      sidebar?.classList.remove("open");
+    } else {
       sidebar?.classList.toggle("open");
-      menuBtnChange(); //calling the function(optional)
-    });
-
-    searchBtn?.addEventListener("click", () => {
-      // Sidebar open when you click on the search icon
-      sidebar?.classList.toggle("open");
-      menuBtnChange(); //calling the function(optional)
-    });
-
-    // following are the code to change sidebar button(optional)
-    function menuBtnChange() {
-      if (sidebar?.classList.contains("open")) {
-        closeBtn?.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
-      } else {
-        closeBtn?.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
-      }
     }
   }
 }
