f5fb45440964a292679a4ebca9c53bff542f9098
nacho <ignacio.bsso@hotmail.com>
2022-04-12 17:01:21 -0300

    add all

--- src/app/public/components/carousel/carousel.component.ts
+++ src/app/public/components/carousel/carousel.component.ts
@@ -1,34 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Slide } from 'src/app/backoffice/models/slide.interface';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent  {
 
-  public items!: any[]
+  @Input() items!: Slide[]
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.items = [
-      {
-        title: 'title1',
-        description: 'description1',
-        img: 'https://i0.wp.com/serranojaimeconsultores.com/wp-content/uploads/2018/03/ong-2.jpg?fit=1024%2C682&ssl=1'
-      },
-      {
-        title: 'title2',
-        description: 'description1',
-        img: 'https://educowebmedia.blob.core.windows.net/educowebmedia/educospain/media/images/blog/ong-y-ods.jpg'
-      },
-      {
-        title: 'title2',
-        description: 'description1',
-        img: 'https://media.istockphoto.com/photos/young-happy-volunteers-outdoor-meeting-at-park-picture-id1145183123?k=20&m=1145183123&s=612x612&w=0&h=FcwNNBTy51IIU1TTeNlvjqOovTly5QnZ6T6Y_W88NSw='
-      }
-    ]
-  }
+
 
 }
--- src/app/public/components/listnews/listnews.component.ts
+++ src/app/public/components/listnews/listnews.component.ts
@@ -1,10 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { News } from "src/app/backoffice/models/news";
+import { NewsService } from "src/app/backoffice/services/news/news.service";
 
 @Component({
   selector: "app-listnews",
   templateUrl: "./listnews.component.html",
   styleUrls: ["./listnews.component.scss"],
 })
-export class ListnewsComponent {
-  constructor() {}
+export class ListnewsComponent implements OnInit {
+  public news!: News[]
+  constructor(private newService: NewsService) {}
+
+  ngOnInit(): void {
+    this.newService.getAll().subscribe(res =>{
+      this.news = res
+    })
+  }
 }
