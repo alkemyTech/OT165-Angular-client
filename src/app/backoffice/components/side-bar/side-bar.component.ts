55989c367c674879298795229ca65207f42712f1
Lorenzo Spallione <82187204+lorenzo0895@users.noreply.github.com>
2022-04-12 12:23:39 -0300

    feat/OT165-123 Crear componente Header para Backoffice (#89)
    * 'add-side-bar'
    
    * 'fix'
    
    * 'finish-side-bar'
    
    * Header Component for backoffice
    
    * Minor changes
    
    * Minor changes before PR
    
    Co-authored-by: nacho <ignacio.bsso@hotmail.com>

--- src/app/backoffice/components/side-bar/side-bar.component.ts
+++ src/app/backoffice/components/side-bar/side-bar.component.ts
@@ -5,36 +5,13 @@ import { Component, Input, OnInit } from "@angular/core";
   templateUrl: "./side-bar.component.html",
   styleUrls: ["./side-bar.component.scss"],
 })
-export class SideBarComponent implements OnInit {
-
-  @Input() itemsList!: any[]
+export class SideBarComponent {
+  @Input() itemsList!: any[];
+  isOpen: boolean = false;
 
   constructor() {}
 
-  ngOnInit(): void {
-    
-    let sidebar = document.querySelector(".sidebar");
-    let closeBtn = document.querySelector("#btn");
-    let searchBtn = document.querySelector(".bx-search");
-
-    closeBtn?.addEventListener("click", () => {
-      sidebar?.classList.toggle("open");
-      menuBtnChange(); //calling the function(optional)
-    });
-
-    searchBtn?.addEventListener("click", () => {
-      // Sidebar open when you click on the search icon
-      sidebar?.classList.toggle("open");
-      menuBtnChange(); //calling the function(optional)
-    });
-
-    // following are the code to change sidebar button(optional)
-    function menuBtnChange() {
-      if (sidebar?.classList.contains("open")) {
-        closeBtn?.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
-      } else {
-        closeBtn?.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
-      }
-    }
+  menuBtnChange() {
+    this.isOpen = !this.isOpen;
   }
 }
