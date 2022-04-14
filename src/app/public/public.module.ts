6ec77b39a52ce0c98c0634910fcbd74160624dfd
Esteban Fernandez <86627778+efernandezdev@users.noreply.github.com>
2022-04-14 13:03:00 -0300

    Feat/ot165 121 Pdf viewer - Terms and conditions (#94)
    * Created modal
    
    * Install ng2-pdf-viewer
    
    * check terms and conditions
    
    * Add colors in buttons modal

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,24 +1,25 @@
 //Own Component
-import { AboutComponent } from './pages/aboutUs/about.component';
-import { ActivitiesListComponent } from './pages/activities/activities-list/activities-list.component';
-import { ActivityCardComponent } from './pages/activities/activity-card/activity-card.component';
-import { ActivityFormComponent } from './pages/activities/activity-form.component';
-import { ButtonModule } from 'primeng/button';
-import { ContactComponent } from './pages/contact/contact.component';
-import { DetailComponent } from './pages/activities/details/detail.component';
-import { DonationAmountComponent } from './pages/donations/donation-amount/donation-amount.component';
-import { DonationComponent } from './pages/donations/donation/donation.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ImageCurvedComponent } from './components/image-curved/image-curved.component';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { InputTextModule } from 'primeng/inputtext';
-import { ListnewsComponent } from './components/listnews/listnews.component';
-import { PublicComponent } from './public.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { SliderComponent } from './components/slider/slider.component';
-import { ThanksComponent } from './pages/donations/thanks/thanks.component';
-import { LoginFormComponent } from './pages/auth/login-form/login-form.component';
-import { RegisterFormComponent } from './pages/auth/register-form/register-form.component';
+import { AboutComponent } from "./pages/aboutUs/about.component";
+import { ActivitiesListComponent } from "./pages/activities/activities-list/activities-list.component";
+import { ActivityCardComponent } from "./pages/activities/activity-card/activity-card.component";
+import { ActivityFormComponent } from "./pages/activities/activity-form.component";
+import { ButtonModule } from "primeng/button";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { DetailComponent } from "./pages/activities/details/detail.component";
+import { DonationAmountComponent } from "./pages/donations/donation-amount/donation-amount.component";
+import { DonationComponent } from "./pages/donations/donation/donation.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ImageCurvedComponent } from "./components/image-curved/image-curved.component";
+import { InputNumberModule } from "primeng/inputnumber";
+import { InputTextModule } from "primeng/inputtext";
+import { ListnewsComponent } from "./components/listnews/listnews.component";
+import { PublicComponent } from "./public.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { SliderComponent } from "./components/slider/slider.component";
+import { ThanksComponent } from "./pages/donations/thanks/thanks.component";
+import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
+import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
+import { TermsandconditionsComponent } from "./components/termsandconditions/termsandconditions.component";
 
 //Own Modules
 import { SharedModule } from "../shared/shared.module";
@@ -28,8 +29,9 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import { GoogleMapsModule } from "@angular/google-maps";
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { NgxTwitterTimelineModule } from 'ngx-twitter-timeline';
+import { LeafletModule } from "@asymmetrik/ngx-leaflet";
+import { PdfViewerModule } from "ng2-pdf-viewer";
+import { NgxTwitterTimelineModule } from "ngx-twitter-timeline";
 
 @NgModule({
   declarations: [
@@ -49,10 +51,9 @@ import { NgxTwitterTimelineModule } from 'ngx-twitter-timeline';
     ActivitiesListComponent,
     LoginFormComponent,
     RegisterFormComponent,
+    TermsandconditionsComponent,
   ],
-  exports: [
-    CarouselModule
-  ],
+  exports: [CarouselModule],
   imports: [
     CarouselModule,
     CommonModule,
@@ -65,6 +66,7 @@ import { NgxTwitterTimelineModule } from 'ngx-twitter-timeline';
     GoogleMapsModule,
     NgxTwitterTimelineModule,
     LeafletModule,
+    PdfViewerModule,
   ],
 })
 export class PublicModule {}
