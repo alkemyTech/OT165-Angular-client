4d0fa71c98680248e75c977cb55eeb154f81a809
DanieLazo17 <daniel.lazo92@gmail.com>
2022-04-16 00:07:38 -0300

    canActivate Users Guards updated

--- src/app/guards/users/users.guard.ts
+++ src/app/guards/users/users.guard.ts
@@ -1,27 +1,42 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { User } from 'src/app/backoffice/models/user';
+import { AppState } from 'src/app/state/app.state';
+import { selectUserData, selectUserToken } from 'src/app/state/selectors/auth.selectors';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersGuard implements CanActivate {
+  userLoged$: Observable<any>;
+  token$: Observable<any>;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private store: Store<AppState>, private router: Router) {
+    this.userLoged$ = this.store.select(selectUserData);
+    this.token$ = this.store.select(selectUserToken);
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      let userLoged$: Observable<any> = this.authService.getUserLoged;
-      let userLoged;
-      userLoged$.subscribe((response) => {
-        userLoged = response
-      })    
-      if ( userLoged != null ) {
-        return true;
-      }
-      this.router.navigate(['home']);
-      return false;
-  } 
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree 
+  {
+    let user!: User;
+    let token!: string;
+
+    this.userLoged$.subscribe((response) => {
+      user = response;
+    });
+    this.token$.subscribe((response) => {
+      token = response;
+    });
+
+    if (user == undefined && token == undefined) {
+      return true;
+    }
+    console.log('User has already logged')
+    this.router.navigate(["home"]);
+    return false;
+  }
 }
