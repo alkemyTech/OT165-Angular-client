1f53152c57443163e2932824c95d430f554e46a0
nacho <ignacio.bsso@hotmail.com>
2022-04-18 17:45:43 -0300

    add button top and how to donate

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, Inject, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { Slide } from "src/app/backoffice/models/slide.interface";
@@ -6,6 +6,7 @@ import { AppState } from "src/app/state/app.state";
 import { selectSlidesListWithOrder } from "src/app/state/selectors/slides.selectors";
 import * as actions from "src/app/state/actions/slides.actions";
 import { DialogService } from "src/app/shared/components/dialog/dialog.service";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "app-home",
@@ -16,13 +17,15 @@ export class HomeComponent implements OnInit {
   isLoading!: boolean;
   slides$: Observable<Slide[]> = new Observable();
   slides: Slide[] = [];
+  public showButton = false;
 
   public welcome: string =
     "En Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio generando procesos de crecimiento y inserción social.";
 
   constructor(
     private store: Store<AppState>,
-    private dialogService: DialogService
+    private dialogService: DialogService,
+    @Inject(DOCUMENT) private document: Document
   ) {
     this.store.dispatch(actions.getSlides());
   }
@@ -47,5 +50,31 @@ export class HomeComponent implements OnInit {
         });
       }
     );
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    if (
+      (yOffSet ||
+        this.document.documentElement.scrollTop ||
+        this.document.body.scrollTop) > 500
+    ) {
+      this.showButton = true;
+    } else if (
+      this.showButton &&
+      (yOffSet ||
+        this.document.documentElement.scrollTop ||
+        this.document.body.scrollTop) < 500
+    ) {
+      this.showButton = false;
+    }
+  }
+
+  onScrollTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
   }
 }
