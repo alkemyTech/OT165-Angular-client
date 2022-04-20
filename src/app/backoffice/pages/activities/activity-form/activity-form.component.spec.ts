c9c63c9cbce6c78c7a258dbf674265a930f5ab56
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-04-20 16:49:48 -0300

    Inputs validators

--- src/app/backoffice/pages/activities/activity-form/activity-form.component.spec.ts
+++ src/app/backoffice/pages/activities/activity-form/activity-form.component.spec.ts
@@ -1,19 +1,24 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  inject,
+  TestBed,
+  waitForAsync,
+} from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 
 import { ActivityFormComponent } from "./activity-form.component";
 import { FormBuilder } from "@angular/forms";
-import { Store, StoreModule } from "@ngrx/store";
+import { StoreModule } from "@ngrx/store";
 import { activityReducer } from "src/app/state/reducers/activity.reducers";
 import { SharedModule } from "src/app/shared/shared.module";
-import { FileUploadModule } from "primeng/fileupload";
-import { HttpClientModule } from "@angular/common/http";
-import { provideMockStore, MockStore } from "@ngrx/store/testing";
-import { ActivityState, IActivity } from "src/app/shared/models/Activity";
+import { provideMockStore } from "@ngrx/store/testing";
+import { ActivityState } from "src/app/shared/models/Activity";
+import { DialogService } from "src/app/shared/components/dialog/dialog.service";
 
 describe("ActivityFormComponent", () => {
   let component: ActivityFormComponent;
   let fixture: ComponentFixture<ActivityFormComponent>;
+  let dialogService: any;
   const initialState: ActivityState = {
     activities: [],
     activity: [],
@@ -29,7 +34,11 @@ describe("ActivityFormComponent", () => {
         StoreModule.forRoot({ activities: activityReducer }),
         SharedModule,
       ],
-      providers: [FormBuilder, provideMockStore({ initialState })],
+      providers: [
+        FormBuilder,
+        provideMockStore({ initialState }),
+        DialogService,
+      ],
     }).compileComponents();
   });
 
@@ -39,49 +48,71 @@ describe("ActivityFormComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create the component", () => {
+  it("[Component] should create the component", () => {
     expect(component).toBeTruthy();
   });
 
-  it("page should show the logo", () => {
+  it("[Logo Component] page should show the logo", () => {
     const nativeElement = fixture.nativeElement;
     expect(nativeElement.querySelector("app-logo")).toBeTruthy();
   });
 
-  it("form should require a name", () => {
-    component.activityForm.setValue({
-      name: "",
-      description: "Hello world",
-      image: "hello.jpg",
-    });
-    expect(component.activityForm.valid).toEqual(false);
+  it("[Form Check] page should render de form", () => {
+    const nativeElement = fixture.nativeElement;
+    expect(nativeElement.querySelector("#formContent")).toBeTruthy();
   });
 
-  it("form should require a description", () => {
-    component.activityForm.setValue({
-      name: "Hello world",
-      description: "",
-      image: "hello.jpg",
-    });
-    expect(component.activityForm.valid).toEqual(false);
+  it("[Name Check] name should be required", () => {
+    const name = component.activityForm.controls["name"];
+    expect(name.valid).toBeFalsy();
+    expect(name.pristine).toBeTruthy();
+    expect(name.errors?.required).toBeTruthy();
+    name.setValue("Test name");
+    expect(name.valid).toBeTruthy();
+    expect(name.errors).toBeNull();
   });
 
-  it("form should require an image", () => {
-    component.activityForm.setValue({
-      name: "Hello world",
-      description: "Hello world",
-      image: "",
-    });
-    expect(component.activityForm.valid).toEqual(false);
+  it("[Description Check] description should be required", () => {
+    const description = component.activityForm.controls["description"];
+    expect(description.valid).toBeFalsy();
+    expect(description.pristine).toBeTruthy();
+    expect(description.errors?.required).toBeTruthy();
+    description.setValue("Test description");
+    expect(description.valid).toBeTruthy();
+    expect(description.errors).toBeNull();
   });
 
-  it("form create an anctivity", () => {
-    component.activityForm.setValue({
-      name: "Actividad de prueba",
-      description: "<p>Descripci&oacute;n de prueba</p>",
-      image: "data:image/png;base64,iVBORw0KGgoNjgCzQg8BVlWbJQ8EukRABNjjKo",
-    });
-    component.createActivity();
-    expect(component.activityForm.valid).toEqual(false);
+  it("[Image Check] image should be required", () => {
+    const image = component.activityForm.controls["image"];
+    expect(image.valid).toBeFalsy();
+    expect(image.pristine).toBeTruthy();
+    expect(image.errors?.required).toBeTruthy();
+    image.setValue("Test image");
+    expect(image.valid).toBeTruthy();
+    expect(image.errors).toBeNull();
   });
+
+  it("[Form Check] if form is valid, it should create and add a success message", inject(
+    [DialogService],
+    (dialogService: DialogService) => {
+      dialogService.messagesObservable.subscribe((el: any) => {
+        console.log("llegué acá");
+        console.log(el);
+      });
+      expect(component.activityForm.invalid).toBeTruthy();
+      component.activityForm.setValue({
+        name: "Actividad de prueba",
+        description: "<p>Descripci&oacute;n de prueba</p>",
+        image: "data:image/png;base64,iVBORw0KGgoNjgCzQg8BVlWbJQ8EukRABNjjKo",
+      });
+      fixture.detectChanges();
+      component.createActivity();
+      // fixture.detectChanges();
+
+      dialogService.messagesObservable.subscribe((el) => {
+        console.log(el);
+        expect(el[0].type).toEqual("success");
+      });
+    }
+  ));
 });
